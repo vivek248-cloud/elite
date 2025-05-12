242e153651a5bc2dcfdcf9889ba77f21edee01df
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-12 16:52:51 +0530

    Update settings.py

--- elite/settings.py
+++ elite/settings.py
@@ -29,7 +29,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=True
     )
 }
 
