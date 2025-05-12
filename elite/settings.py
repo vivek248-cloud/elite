13d3bb88bbf428b81498e1c7ef66bf6118ccf572
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-12 13:30:49 +0530

    Update settings.py

--- elite/settings.py
+++ elite/settings.py
@@ -1,142 +1,5 @@
 
 
-# import dj_database_url
-
-# import os
-# from pathlib import Path
-
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SECRET_KEY = 'your-secret-key-here'
-# DEBUG = False
-# ALLOWED_HOSTS = ['*']
-
-# INSTALLED_APPS = [
-#     "admin_interface",
-#     "colorfield",#FOR ADMIN INTERFACE
-    
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'index',
-   
-    
-# ]
-
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
-# ]
-
-
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
-# ROOT_URLCONF = 'elite.urls'
-
-# # LOGGINGS
-
-# LOG_DIR = os.path.join(BASE_DIR, 'logs')
-# if not os.path.exists(LOG_DIR):
-#     os.makedirs(LOG_DIR)
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {
-#             'format': '{levelname} {asctime} {module} {message}',
-#             'style': '{',
-#         },
-#         'simple': {
-#             'format': '{levelname} {message}',
-#             'style': '{',
-#         },
-#     },
-#     'handlers': {
-#         'file': {
-#             'level': 'ERROR',  # Logs only ERROR messages
-#             'class': 'logging.FileHandler',
-#             'filename': BASE_DIR / 'logs/django_errors.log',  # Saves logs in "logs" folder
-#             'formatter': 'verbose',
-#         },
-#         'console': {
-#             'level': 'DEBUG',  # Logs all messages to console
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'simple',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file', 'console'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#     },
-# }
-
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'templates'],  
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'elite.wsgi.application'
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'BASE_DIR / db.sqlite3',
-       
-#     }
-# }
-
-# DATABASES['default'] = dj_database_url.parse("postgresql://root:RKpfaKuy75xs6FP1TKL45u3E5RZ4cYtc@dpg-d0gp20ruibrs73fqv6m0-a/elite_db_ag7l")
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'elite', 'static',)]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# if not DEBUG:
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# EMAIL_FILE_PATH = 'emails'  # Emails will be saved in this folder
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'  # Use your SMTP provider
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'kuttyvivek248@gmail.com'
-# EMAIL_HOST_PASSWORD = 'obka rnfs hcag fsul'  # Use environment variables instead for security
-
-
-
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 
 
@@ -221,6 +84,20 @@ LOGGING = {
             'propagate': True,
         },
     },
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'elite_db_ag7l'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'RKpfaKuy75xs6FP1TKL45u3E5RZ4cYtc'),
+        'HOST': os.getenv('DB_HOST', 'dpg-d0gp20ruibrs73fqv6m0-a.oregon-postgres.render.com'),
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
 }
 
 TEMPLATES = [
