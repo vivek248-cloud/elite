22c0c92b61bc307072a0c0ca0d9d78e5e7f99396
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-13 12:31:49 +0530

    Update settings.py

--- elite/settings.py
+++ elite/settings.py
@@ -239,10 +239,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
+    'django.contrib.staticfiles',
     'cloudinary',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    
 
     'index',
 ]
