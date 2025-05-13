8d8e8e4cd906d3a7e4544a494b30e3f71463cf09
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-13 13:14:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-
 #!/bin/bash
 
-# Install required dependencies
+cd $RENDER_PROJECT_ROOT
+
+# Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Run the server (optional for local development, but for deployment, you may use a production server like gunicorn)
-python manage.py runserver 0.0.0.0:8000
+# Apply migrations
+python manage.py migrate
+
+# Create superuser
+python manage.py shell < create_superuser.py
