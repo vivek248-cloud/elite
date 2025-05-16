84c4eb4b18ad97f269c90f5737007686a2f0479c
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-16 14:40:24 +0530

    Update forms.py

--- index/forms.py
+++ index/forms.py
@@ -1,13 +1,7 @@
 from django import forms
-from .models import QuoteRequest, BudgetRange
+from .models import QuoteRequest
 
 class QuoteRequestForm(forms.ModelForm):
-    budget_range = forms.ModelChoiceField(
-        queryset=BudgetRange.objects.all(),
-        empty_label="Select Budget",
-        widget=forms.Select(attrs={'class': 'form-control'})
-    )
-
     class Meta:
         model = QuoteRequest
-        fields = ['name', 'email', 'phone', 'service_type', 'budget_range']
+        fields = ['name', 'email', 'phone', 'service_type']  # No 'budget_range'
