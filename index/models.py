fb55465c449469b76074d09f25ee5824e9ef91a0
vivek248-cloud <vivekofficial248@gmail.com>
2025-05-24 15:56:02 +0530

    Update models.py

--- index/models.py
+++ index/models.py
@@ -9,8 +9,8 @@ class HomeSlider(models.Model):
     sub_headline = models.CharField(max_length=255)
     quotes = models.TextField(blank=True, null=True)  # Optional quotes field
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='home_slider/')
-    # image = CloudinaryField('image', folder='home_slider')
+    # image = models.ImageField(upload_to='home_slider/')
+    image = CloudinaryField('image', folder='home_slider')
     description = models.TextField(blank=True, null=True)  # Optional description
     location = models.CharField(max_length=255, null=True, blank=True)  # ✅ New field
     created_at = models.DateTimeField(auto_now_add=True)
@@ -100,7 +100,7 @@ class Project(models.Model):
     title = models.CharField(max_length=255)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     description = models.TextField()
-    image = models.ImageField(upload_to='projects/')
+    image = CloudinaryField('image',folder='projects-img')
     location = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     date = models.DateField(default=datetime.date.today)
@@ -125,9 +125,8 @@ class UpcomingProject(models.Model):
     ]
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='projects/upcoming/')
    
-    # image = CloudinaryField('image',folder='upcomming-projects-img')
+    image = CloudinaryField('image',folder='upcomming-projects-img')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ongoing')  # ✅ NEW FIELD
     location = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -144,8 +143,8 @@ class UpcomingProject(models.Model):
 
 class AboutVideo(models.Model):
     title = models.CharField(max_length=255)
-    video_file = models.FileField(upload_to='videos/', null=True, blank=True)
-    # video_file = CloudinaryField('video',resource_type='video',folder='about-video',null=True,blank=True)
+    # video_file = models.FileField(upload_to='videos/', null=True, blank=True)
+    video_file = CloudinaryField('video',resource_type='video',folder='about-video',null=True,blank=True)
 
 
     def __str__(self):
@@ -155,8 +154,8 @@ class AboutVideo(models.Model):
 
 class GalleryImage(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="gallery/")
-    # image = CloudinaryField('image',folder='gallery-img')
+    # image = models.ImageField(upload_to="gallery/")
+    image = CloudinaryField('image',folder='gallery-img')
 
     def __str__(self):
         return self.title
@@ -165,8 +164,8 @@ class GalleryImage(models.Model):
 class AboutSection(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to="about_images/")
-    # image = CloudinaryField('image',folder='team')
+    # image = models.ImageField(upload_to="about_images/")
+    image = CloudinaryField('image',folder='team')
     author_name = models.CharField(max_length=100)
     author_designation = models.CharField(max_length=100)
 
@@ -189,12 +188,12 @@ class Video(models.Model):
     
 class ProjectVideo(models.Model):
     title = models.CharField(max_length=255)
-    video_file = models.FileField(upload_to='projects/videos/')  # Stores uploaded videos
-    # video_file = CloudinaryField(
-    #     'video',
-    #     resource_type='video',
-    #     folder='projects-videos'  # 👈 this creates/uses a Cloudinary folder named "contact-slider"
-    # )
+    # video_file = models.FileField(upload_to='projects/videos/')  # Stores uploaded videos
+    video_file = CloudinaryField(
+        'video',
+        resource_type='video',
+        folder='projects-videos'  # 👈 this creates/uses a Cloudinary folder named "contact-slider"
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -215,12 +214,12 @@ class YouTubeVideo(models.Model):
     
 class SliderVideo(models.Model):
     title = models.CharField(max_length=255)
-    video_file = models.FileField(upload_to='contact-slider/')  # Stores uploaded videos
-    # video_file = CloudinaryField(
-    #     'video',
-    #     resource_type='video',
-    #     folder='slider-videos'  # 👈 this creates/uses a Cloudinary folder named "contact-slider"
-    # )
+    # video_file = models.FileField(upload_to='contact-slider/')  # Stores uploaded videos
+    video_file = CloudinaryField(
+        'video',
+        resource_type='video',
+        folder='slider-videos'  # 👈 this creates/uses a Cloudinary folder named "contact-slider"
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -228,12 +227,12 @@ class SliderVideo(models.Model):
     
 class AboutTitleVideo(models.Model):
     title = models.CharField(max_length=255)
-    video_file = models.FileField(upload_to='about-head/')  # Stores uploaded videos
-    # video_file = CloudinaryField(
-    #     'video',
-    #     resource_type='video',
-    #     folder='AboutTitle-videos'  # 👈 this creates/uses a Cloudinary folder named "contact-slider"
-    # )
+    # video_file = models.FileField(upload_to='about-head/')  # Stores uploaded videos
+    video_file = CloudinaryField(
+        'video',
+        resource_type='video',
+        folder='AboutTitle-videos'  # 👈 this creates/uses a Cloudinary folder named "contact-slider"
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
