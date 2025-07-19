94058386d74b1d41c061594f77c637bb61b6f3ed
Vivek248-cloud <vivekofficial248.com>
2025-07-19 16:40:54 +0530

    new updates

--- index/migrations/0032_project_slug_upcomingproject_slug.py
+++ index/migrations/0032_project_slug_upcomingproject_slug.py
@@ -1,14 +1,32 @@
 from django.db import migrations, models
-import index.utils
+from django.utils.text import slugify
 
+def generate_unique_slug(model, field_value):
+    slug = slugify(field_value)
+    unique_slug = slug
+    num = 1
+
+    while model.objects.filter(slug=unique_slug).exists():
+        unique_slug = f'{slug}-{num}'
+        num += 1
+
+    return unique_slug
 
 def populate_slugs(apps, schema_editor):
     Project = apps.get_model('index', 'Project')
+    UpcomingProject = apps.get_model('index', 'UpcomingProject')
+
     for project in Project.objects.all():
-        if not getattr(project, 'slug', None):
-            project.slug = index.utils.generate_unique_slug(Project, project.name)
+        if not project.slug:
+            project.slug = generate_unique_slug(Project, project.title)
             project.save()
 
+    for up in UpcomingProject.objects.all():
+        if not hasattr(up, 'slug'):
+            continue  # Safety check in case field didn't get added
+        if not up.slug:
+            up.slug = generate_unique_slug(UpcomingProject, up.title)
+            up.save()
 
 class Migration(migrations.Migration):
 
@@ -22,11 +40,15 @@ class Migration(migrations.Migration):
             name='slug',
             field=models.SlugField(blank=True),
         ),
-        migrations.RunPython(populate_slugs),  # Populate existing projects with slug
+        migrations.AddField(
+            model_name='upcomingproject',
+            name='slug',
+            field=models.SlugField(blank=True),
+        ),
+        migrations.RunPython(populate_slugs),
         migrations.AlterField(
             model_name='project',
             name='slug',
             field=models.SlugField(unique=True, blank=True),
         ),
-     
     ]
