36e4d9d86ae8425e5ef3c1809a38fae6619b2807
Vivek248-cloud <vivekofficial248.com>
2025-08-06 15:35:50 +0530

    new updates 01

--- elite/urls.py
+++ elite/urls.py
@@ -1,39 +1,23 @@
-
-
-import os
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from index.views import custom_404
-from index.views import custom_500
-from index.views import serve_media
-from django.conf.urls.i18n import i18n_patterns
+from index.views import custom_404, custom_500, serve_media
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('', include('index.urls')),  # Include index app URLs
+    path('', include('index.urls')),
 ]
 
+# Custom error handlers
 handler404 = 'index.views.custom_404'
-
 handler500 = 'index.views.custom_500'
 
-# Serve media files in development
+# Development static and media serving
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve_media),
-    ]
-# urlpatterns += [
-#     path('i18n/', include('django.conf.urls.i18n')),
-# ]
+else:
+    # Media serving fallback in production (not recommended)
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve_media)]
